3832edb836a0669e5d696a97665854ab269a2cb9
arifjawad <arifjawad.ajp@gmail.com>
2021-09-12 15:36:11 +0600

    error fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,14 +7,14 @@ const Footer = () => {
 <section class="">
  
     <div class="footer_bottom">
-        <a href="">github</a>
-        <a href="">linkedIn</a>
-        <a href="">facebook</a>
-        <a href="">instagram</a>
-        <a href="">patreon</a>
-        <a href="">medium</a><br/>
+        <a href="#">github</a>
+        <a href="#">linkedIn</a>
+        <a href="#">facebook</a>
+        <a href="#">instagram</a>
+        <a href="#">patreon</a>
+        <a href="#">medium</a><br/>
        <div class="f-pd1">
-        <a  href="">copyright &copy; arif-jawad.com</a>
+        <a  href="#">copyright &copy; arif-jawad.com</a>
        </div>
     </div>
  
